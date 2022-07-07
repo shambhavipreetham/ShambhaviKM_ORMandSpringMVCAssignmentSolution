4eab97620e0e864dd6ff29e3733070afde85c66a
Shambhavi K M <shambhavimahadev12@gmail.com>
2022-07-07 13:01:24 +0530

    initial commit

---
+++ d2-db/src/main/resources/templates/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('okay', 'computing', 'shambhavi@gmail.com'),
+  	('spring', 'h2db', 'spring.h2db@gmail.com'),
+  	('springboot', 'in-memory-db', 'springboot@gmail.com');
+  	
---
+++ d2-db/src/test/java/com/h2/db/D2DbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
